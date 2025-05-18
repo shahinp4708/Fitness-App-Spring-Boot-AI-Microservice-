6c1550f1cfb2bd78ba398b74f11de690dc264da7
shahinp4708 <shahinp4708@gmail.com>
2025-05-18 15:53:30 +0530

    implemented keycloak , OAuth2

--- aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -1,13 +1,21 @@
 package com.fitness.aiservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiserviceApplication {
+//	@PostConstruct
+//	public void printEnv() {
+//		System.out.println("GEMINI_API_URL = " + System.getenv("GEMINI_API_URL"));
+//		System.out.println("GEMINI_API_KEY = " + System.getenv("GEMINI_API_KEY"));
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiserviceApplication.class, args);
+
+
 	}
 
 }
---
+++ gateway/src/main/java/com/fitness/gateway/KeycloakUserSyncFilter.java
@@ -0,0 +1,2 @@
+package com.fitness.gateway;public class KeycloakUserSyncFilter {
+}
